439a3b4146c695a67373afe96100b35dec401c89
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-04 15:14:43 +0600

    add zod middlewwire

--- src/app/modules/student/student.zodValidation.ts
+++ src/app/modules/student/student.zodValidation.ts
@@ -1,122 +1,54 @@
-
 import { z } from 'zod';
 
-// Validation for UserName
 const userNameValidationSchema = z.object({
   firstName: z
-    .string({
-      required_error: 'First name is required'
-    })
-    .min(2, 'First name must be at least 2 characters long'),
-
-  middleName: z.string().optional(),
-
-  lastName: z
-    .string({
-      required_error: 'Last name is required'
-    })
-    .min(2, 'Last name must be at least 2 characters long'),
+    .string()
+    .min(1)
+    .max(20)
+    .refine((value) => /^[A-Z]/.test(value), {
+      message: 'First Name must start with a capital letter',
+    }),
+  middleName: z.string(),
+  lastName: z.string(),
 });
 
-// Validation for Guardian
 const guardianValidationSchema = z.object({
-  fatherName: z.string({
-    required_error: 'Father’s name is required'
-  }),
-
-  fatherOccupation: z.string({
-    required_error: 'Father’s occupation is required'
-  }),
-
-  fatherContactNo: z.string({
-    required_error: 'Father’s contact number is required'
-  }),
-
-  motherName: z.string({
-    required_error: 'Mother’s name is required'
-  }),
-
-  motherOccupation: z.string({
-    required_error: 'Mother’s occupation is required'
-  }),
-
-  motherContactNo: z.string({
-    required_error: 'Mother’s contact number is required'
-  }),
+  fatherName: z.string(),
+  fatherOccupation: z.string(),
+  fatherContactNo: z.string(),
+  motherName: z.string(),
+  motherOccupation: z.string(),
+  motherContactNo: z.string(),
 });
 
-// Validation for LocalGuardian
 const localGuardianValidationSchema = z.object({
-  Name: z.string({
-    required_error: 'Local guardian’s name is required'
-  }),
+  name: z.string(),
+  occupation: z.string(),
+  contactNo: z.string(),
+  address: z.string(),
+});
 
-  Occupation: z.string({
-    required_error: 'Local guardian’s occupation is required'
-  }),
-
-  ContactNo: z.string({
-    required_error: 'Local guardian’s contact number is required'
+export const createStudentValidationSchema = z.object({
+  body: z.object({
+    password: z.string().max(20),
+    student: z.object({
+      name: userNameValidationSchema,
+      gender: z.enum(['male', 'female', 'other']),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email(),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      bloogGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      admissionSemester: z.string(),
+      profileImg: z.string(),
+    }),
   }),
 });
 
-// Validation for Student
-const studentValidationSchema = z.object({
-  id: z.string({
-    required_error: 'ID is required'
-  }),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
-
-  name: userNameValidationSchema,
-
-  gender: z.enum(['female', 'male'], {
-    required_error: 'Gender is required'
-  }),
-
-  dateOfBirth: z.string({
-    required_error: 'Date of birth is required'
-  }),
-
-  email: z
-    .string({
-      required_error: 'Email is required'
-    })
-    .email('Invalid email format'),
-
-  contactNo: z.string({
-    required_error: 'Contact number is required'
-  }),
-
-  emergencyContactNo: z.string({
-    required_error: 'Emergency contact number is required'
-  }),
-
-  bloodGroup: z
-    .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], {
-      invalid_type_error: 'Blood group must be a valid type',
-    })
-    .optional(),
-
-  presentAddress: z.string({
-    required_error: 'Present address is required'
-  }),
-
-  permanentAddress: z.string({
-    required_error: 'Permanent address is required'
-  }),
-
-  guardian: guardianValidationSchema,
-
-  localGuardian: localGuardianValidationSchema,
-
-  profileImage: z.string().optional(),
-
-  isActive: z
-    .enum(['active', 'blocked'], {
-      required_error: 'Status is required',
-    })
-    .default('active'),
-  isDeleted: z.boolean().default(false),
-});
-
-export { studentValidationSchema };
+export const studentValidations = {
+  createStudentValidationSchema,
+};
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserController } from './user.controller';
+import { AnyZodObject } from 'zod';
+import { createStudentValidationSchema } from '../student/student.zodValidation';
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudent);
+const validateMiddlewire = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    //VALIDATION check
+    //if everything all right next() ->
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      return next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+router.post(
+  '/create-student',
+  validateMiddlewire(createStudentValidationSchema),
+  UserController.createStudent,
+);
 
 export const UserRoutes = router;
