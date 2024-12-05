ebc5867ab60a44216f091c459a8280c1c5e06ba6
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-06 02:48:39 +0600

    Create validation in faculty

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+import { TAcademicFaculty } from './academicFaculty.interface';
+
+const academicFaultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+export const AcademicFaculty = model<TAcademicFaculty>(
+    'AcademicFaculty',
+    academicFaultySchema
+)
