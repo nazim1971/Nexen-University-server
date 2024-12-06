6586588bbf68a9c4e9e024cf3aa2ab178711035d
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-07 00:01:28 +0600

    Create academic department

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,16 +2,17 @@ import { Router } from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { StudentRoutes } from '../modules/student/student.route';
 import { AcademicSemesterRoutes } from '../modules/academmicSemester.ts/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 
 const router = Router();
 
 const moduleRoutes = [
   { path: '/user', route: UserRoutes },
   { path: '/student', route: StudentRoutes },
-  { path: '/academic-semester', route: AcademicSemesterRoutes}
+  { path: '/academic-semester', route: AcademicSemesterRoutes },
+  { path: '/academic-faculty', route: AcademicFacultyRoutes },
 ];
 
-
-moduleRoutes.forEach((route)=> router.use(route.path, route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
