d93d42a780aa1af5ba806b2f6df841f3cd1521c3
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-12 21:20:51 +0600

    Create semester registraytion model

---
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -0,0 +1,40 @@
+import { Schema } from 'mongoose';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { semesterRegistrationStatus } from './semesterRegistration.const';
+
+const semesterRegistrationSchema = new Schema<TSemesterRegistration>(
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: semesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startData: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+      required: true,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
