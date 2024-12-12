81f8fbd676841fda2e72d336ad91c8495cdc5663
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-12 22:07:52 +0600

    create acdemic semester check

--- src/app/modules/semesterRegistration/semesterRegistration.services.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.services.ts
@@ -1,16 +1,27 @@
+import  httpStatus  from 'http-status';
+import { AppError } from '../../errors/AppError';
+import { academicSemester } from '../academmicSemester.ts/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 
-const createSemesterRegistrationIntoDB = async (payload: <TSemesterRegistration>)=>{
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {
+  //check if the semester is Exist
+  const academicSemesters = payload?.academicSemester;
+  const isAcademicSemesterExists =
+    await academicSemester.findById(academicSemesters);
+    if(!isAcademicSemesterExists){
+        throw new AppError(httpStatus.NOT_FOUND, "This academic semester not found !!!" )
+    }
+};
 
-}
-
-const getAllSemesterRegistrationFromDB = async () => {}
-const getSingleSemesterRegistrationFromDB = async () => {}
-const updateSemesterRegistrationFromDB = async () => {}
+const getAllSemesterRegistrationFromDB = async () => {};
+const getSingleSemesterRegistrationFromDB = async () => {};
+const updateSemesterRegistrationFromDB = async () => {};
 
 export const SemesterRegistrationService = {
-    createSemesterRegistrationIntoDB,
-    getAllSemesterRegistrationFromDB,
-    getSingleSemesterRegistrationFromDB,
-    updateSemesterRegistrationFromDB
-}
+  createSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationFromDB,
+  getSingleSemesterRegistrationFromDB,
+  updateSemesterRegistrationFromDB,
+};
