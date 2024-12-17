ebbf8bf0d8c501713da759d9987af85e13b549c9
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-17 11:45:58 +0600

    Create user static mathod

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -5,31 +5,29 @@ import httpStatus from 'http-status';
 import bcrypt from 'bcrypt'
 
 const loginUser = async (payload: TLoginUser) => {
-  const isUserExist = await User.findOne({
-    id: payload?.id,
-  });
+  // const isUserExist = 
 
-  if (!isUserExist) {
+  if (!User.isUserExistsByCustomId(payload.id)) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!!!');
   }
 
-  const isDeleted = isUserExist?.isDeleted;
-  // check if the user is already deleted
-  if (isDeleted) {
-    throw new AppError(httpStatus.FORBIDDEN, 'This user is not found!!!');
-  }
+  // const isDeleted = isUserExist?.isDeleted;
+  // // check if the user is already deleted
+  // if (isDeleted) {
+  //   throw new AppError(httpStatus.FORBIDDEN, 'This user is not found!!!');
+  // }
 
-  const userStatus = isUserExist?.status;
-  // check if the user is already deleted
-  if (userStatus === "blocked") {
-    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!!!');
-  }
+  // const userStatus = isUserExist?.status;
+  // // check if the user is already deleted
+  // if (userStatus === "blocked") {
+  //   throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!!!');
+  // }
 
-  //check if the pass is correct
+  // //check if the pass is correct
 
-  const isPassMatch = await bcrypt.compare(payload.password, isUserExist?.password)
+  // const isPassMatch = await bcrypt.compare(payload.password, isUserExist?.password)
 
-  console.log(isPassMatch);
+  // console.log(isPassMatch);
 };
 
 export const AuthService = {
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,57 +1,58 @@
-import { model, Schema } from "mongoose";
-import { Tuser } from "./user.interface";
-import config from "../../config";
-import bcrypt from 'bcrypt'
-
+import { model, Schema } from 'mongoose';
+import { Tuser } from './user.interface';
+import config from '../../config';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<Tuser>(
-    {
-      id: {
-        type: String,
-        required: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      needPasswordChange: {
-        type: Boolean,
-        default: true,
-      },
-      role: {
-        type: String,
-        enum: ['student', 'faculty', 'admin'],
-      },
-      status: {
-        type: String,
-        enum: ['in-progress', 'blocked'],
-        default: 'in-progress',
-      },
-      isDeleted: {
-        type: Boolean,
-        default: false,
-      },
+  {
+    id: {
+      type: String,
+      required: true,
     },
-    {
-      timestamps: true,
+    password: {
+      type: String,
+      required: true,
     },
-  );
+    needPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'faculty', 'admin'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-  userSchema.pre('save', async function (next) {
-    
-    // const user = this; // doc
-    // hashing password and save into DB
-    this.password = await bcrypt.hash(
-     this.password,
-      Number(config.slat),
-    );
-    next();
-  });
-  
-  // set '' after saving password
-  userSchema.post('save', function (doc, next) {
-    doc.password = '';
-    next();
-  });
+userSchema.pre('save', async function (next) {
+  // const user = this; // doc
+  // hashing password and save into DB
+  this.password = await bcrypt.hash(this.password, Number(config.slat));
+  next();
+});
 
-  export const User = model<Tuser>('User', userSchema)
+// set '' after saving password
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+userSchema.statics.isUserExistByCustomId = async function (id: string) {
+  return await User.findOne({
+    id,
+  });
+};
+
+export const User = model<Tuser>('User', userSchema);
