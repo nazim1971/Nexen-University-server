b7a5a109bc71bfcdc5002f239f2c3c46d59bb993
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-23 13:59:54 +0600

    Add forget pass and reset pass route

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
 import { validateMiddlewire } from '../../utils/validateRequest';
@@ -7,14 +7,35 @@ import { USER_ROLE } from '../user/user.const';
 
 const router = express.Router();
 
-router.post('/login',validateMiddlewire(AuthValidation.loginValidationSchema) ,AuthController.loginUser)
+router.post(
+  '/login',
+  validateMiddlewire(AuthValidation.loginValidationSchema),
+  AuthController.loginUser,
+);
 
-router.post('/change-password', auth(USER_ROLE.student, USER_ROLE.faculty, USER_ROLE.admin) , validateMiddlewire(AuthValidation.changePassValidationSchema) ,AuthController.changePassword)
+router.post(
+  '/change-password',
+  auth(USER_ROLE.student, USER_ROLE.faculty, USER_ROLE.admin),
+  validateMiddlewire(AuthValidation.changePassValidationSchema),
+  AuthController.changePassword,
+);
 
-router.post('/refresh-token',
-    validateMiddlewire(AuthValidation.refreshTokenValidationSchema),
-    AuthController.refreshToken,)
+router.post(
+  '/refresh-token',
+  validateMiddlewire(AuthValidation.refreshTokenValidationSchema),
+  AuthController.refreshToken,
+);
 
-router.post('/forget-password', )    
+router.post(
+  '/forget-password',
+  validateMiddlewire(AuthValidation.forgetPasswordValidationSchema),
+  AuthController.forgetPassword,
+);
+
+router.post(
+  '/reset-password',
+  validateMiddlewire(AuthValidation.forgetPasswordValidationSchema),
+  AuthController.resetPassword,
+);
 
 export const AuthenticationRoute = router;
