25a599b7390742fbedae72a2c50bcc0c228b1e59
nazim1971 <md.nazimuddinaj@gmail.com>
2024-12-29 02:43:58 +0600

    Handle enrolled course error

--- src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
@@ -1,7 +1,7 @@
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { EnrolledCourseService } from "./enrolledCourse.service";
-
+import httpStatus from "http-status";
 
 const createEnrolledCourse = catchAsync(
     async (req, res) => {
